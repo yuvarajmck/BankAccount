3d698df7899c1e1280de87ab69be106496f0f90e
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-16 18:20:59 +0530

    Final product

--- src/AccessAccount.java
+++ src/AccessAccount.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class AccessAccount {
@@ -12,86 +11,100 @@ public class AccessAccount {
     private void getAccountDetails(AccountDetails accountDetails){
         Scanner userInput = new Scanner(System.in);
         String name = accountDetails.getName();
-        System.out.println("Hello " + name);
-        while(true){
+        int accNo = accountDetails.getAccountNumber();
+        System.out.println("\nName : " + name);
+        System.out.println("Account Number : "+accNo);
+        while(true) {
+            try {
+                double bal = accountDetails.getBalance();
+                System.out.println("\nType 1 -> Balance enquiry\nType 2 -> Withdraw\nType 3 -> Deposit\nType 4 -> Change pin\nType 0 -> Exit");
+                int choice = userInput.nextInt();
+                switch (choice) {
+                    case 1 -> {
+                        System.out.println("Balance : " + bal);
+                    }
+                    case 2 -> {
+                        System.out.print("Enter an amount to withdraw : ");
 
-            double bal = accountDetails.getBalance();
-            System.out.println("Type 1 -> Balance enquiry\nType 2 -> Withdraw\nType 3 -> Deposit\nType 4 -> Change pin\nType 0 -> Exit");
-            int choice = userInput.nextInt();
-            switch (choice) {
-                case 1 -> {
-                    System.out.println("Balance : " + bal);
-                }
-                case 2 -> {
-                    System.out.print("Enter an amount to withdraw : ");
-
-                    try {
-                        double amt = userInput.nextDouble();
-                        if (amt <= accountDetails.getBalance() && amt > 10) {
-                            accountDetails.withdrawMoney(amt);
-                            System.out.println("\nKindly tak your cash and visit again....");
+                        try {
+                            double amt = userInput.nextDouble();
+                            if (amt <= accountDetails.getBalance() && amt > 10) {
+                                accountDetails.withdrawMoney(amt);
+                                System.out.println("\nKindly take your cash and visit again....\n");
+                            } else {
+                                System.out.println("Insufficient Balance\n");
+                            }
+                        } catch (Exception e) {
+                            System.out.print("\nInvalid Input");
                         }
-                    } catch (Exception e) {
-                        System.out.print("\nInvalid Input");
                     }
-                }
-                case 3 -> {
-                    System.out.print("Enter the money to deposit : ");
-                    try {
-                        double amt = userInput.nextDouble();
-                        if (amt > 10 && amt < 1_000_000) {
-                            accountDetails.depositMoney(amt);
-                            System.out.println("\nDeposited successfully");
+                    case 3 -> {
+                        System.out.print("\nEnter the money to deposit : ");
+                        try {
+                            double amt = userInput.nextDouble();
+                            if (amt > 10 && amt < 1_000_000) {
+                                accountDetails.depositMoney(amt);
+                                System.out.println("\nDeposited successfully");
+                            } else {
+                                System.out.println("Enter amount in the range of 10 - 1,000,000.");
+                            }
+                        } catch (Exception e) {
+                            System.out.println("\nInvalid Input");
                         }
-                    } catch (Exception e) {
-                        System.out.println("\nInvalid Input");
+                    }
+                    case 4 -> {
+                        System.out.print("Enter New pin : ");
+                        String pin = userInput.next();
+                        if (pin.length() == 6) {
+                            System.out.println("Pin Updated successfully");
+                        } else {
+                            System.out.println("Invalid Pin");
+                        }
+                    }
+                    case 0 -> {
+                        return;
+                    }
+                    default -> {
+                        System.out.println("Invalid Option");
                     }
                 }
-                case 4 -> {
-                    System.out.print("Enter New pin : ");
-                    String pin = userInput.nextLine();
-                    if (pin.length() == 6) {
-                        System.out.println("Pin Updated successfully");
-                    } else {
-                        System.out.println("Invalid Pin");
-                    }
-                }
-                case 0 -> {
-                    return;
-                }
-                default -> {
-                    System.out.println("Invalid Option");
-                }
+            }catch (Exception e){
+                System.out.println("Invalid Input");
             }
         }
 
     }
     private void validateAccount(ArrayList<AccountDetails> accountDetails){
         Scanner userInput = new Scanner(System.in);
-        while(true){
-            System.out.println("Enter account Number : ");
-
-            int accountNo = userInput.nextInt();
+        while(true) {
+            try {
 
 
-            for (AccountDetails i : accountDetails) {
-                if (i.getAccountNumber() == accountNo) {
-                    System.out.print("\nEnter pin : ");
-                    String Pin = userInput.next();
-                    System.out.println(i.getPin().equals(Pin));
-                    if (i.getPin().equals(Pin)) {
-                        getAccountDetails(i);
-                        return;
-                    }
-                    else{
+                System.out.println("Enter account Number : ");
 
+                int accountNo = userInput.nextInt();
+
+
+                for (AccountDetails i : accountDetails) {
+                    if (i.getAccountNumber() == accountNo) {
+                        System.out.print("\nEnter pin : ");
+                        String Pin = userInput.next();
+                        System.out.println(i.getPin().equals(Pin));
+                        if (i.getPin().equals(Pin)) {
+                            getAccountDetails(i);
+                            return;
+                        } else {
+
+                        }
                     }
                 }
-            }
-            System.out.println("Type 0 -> Exit");
-            int choice = userInput.nextInt();
-            if(choice==0){
-                return;
+                System.out.println("Type 0 -> Exit");
+                int choice = userInput.nextInt();
+                if (choice == 0) {
+                    return;
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid Input");
             }
         }
     }
--- src/CreateAccount.java
+++ src/CreateAccount.java
@@ -20,7 +20,7 @@ public class CreateAccount {
         pin = userInput.nextLine();
         if(pin.length()==6){
             int accNo = (int)(Math.random()*1_000_000);
-            System.out.println("Account created successfully...");
+            System.out.println("Account created successfully...\n");
             System.out.println("Name : "+name);
             System.out.println("Account Number : "+accNo);
             AccountDetails accInfo = new AccountDetails();
@@ -29,7 +29,7 @@ public class CreateAccount {
             accInfo.setName(name);
             accInfo.depositMoney(0);
             accountDetails.add(accInfo);
-            System.out.println("account Created successfully");
+            System.out.println();
         }
         else{
             System.out.println("Must be a 6 digit number");
